1801cee8590dc1a448c30e25f9dfc47e2cc14804
{km} <{k3042227625n@gmail.com}>
2023-10-02 12:11:37 +0900

    PDOを使ってMySQLデータに接続

---
+++ dbc.php
@@ -0,0 +1,18 @@
+<?php
+
+$dsn = 'mysql:host=localhost;dbname=blog_app;charset=utf8';
+$user = 'root';
+$pass = 'root';
+
+try {
+    $dbh = new PDO($dsn,$user,$pass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+    echo '接続成功';
+    $dbh = null;
+} catch(PDOException $e) {
+    echo '接続失敗'. $e->getMessage();
+    exit();
+};
+
+?>
