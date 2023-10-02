6290ea8957af546e84d1b9b4d22242cb2170ff73
{km} <{k3042227625n@gmail.com}>
2023-10-02 09:57:57 +0900

    first commit

---
+++ blog.php
@@ -0,0 +1,17 @@
+<?php
+
+$blog = $_POST;
+
+if ($blog['publish_status'] === 'publish') {
+    foreach($blog as $key => $value) {
+        echo '<pre>';
+        echo $key . ' : '. htmlspecialchars($value,ENT_QUOTES,'UTF-8');
+        echo '<pre>';
+    }
+} elseif($blog['publish_status'] === 'un_publish') {
+    echo '公開中の記事がありません。';
+} else {
+    echo '記事がありません。';
+}
+
+?>
