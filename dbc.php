75538355b8818bfa8a372aa83fb60b645c387fa8
{km} <{k3042227625n@gmail.com}>
2023-10-05 07:00:43 +0900

    データベースからブログ内容を取り出す

--- dbc.php
+++ dbc.php
@@ -8,7 +8,13 @@ try {
     $dbh = new PDO($dsn,$user,$pass, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]);
-    echo '接続成功';
+    // ①SQLの準備
+    $sql = 'SELECT * FROM blog';
+    // ②SQLの実行(queryでSQLステートメントを実行し、結果セットをPDOStatementオブジェクトとして返す)
+    $stmt = $dbh->query($sql);
+    // ③SQLの結果を受け取る
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    // var_dump($result);
     $dbh = null;
 } catch(PDOException $e) {
     echo '接続失敗'. $e->getMessage();
@@ -16,3 +22,30 @@ try {
 };
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ブログ一覧</title>
+</head>
+<body>
+    <h2>ブログ一覧</h2>
+    <table>
+        <tr>
+            <th>No</th>
+            <th>タイトル</th>
+            <th>カテゴリ</th>
+        </tr>
+        <?php foreach($result as $column): ?>
+        <tr>
+            <td><?php echo $column['id'] ?></td>
+            <td><?php echo $column['title'] ?></td>
+            <td><?php echo $column['category'] ?></td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+    
+</body>
+</html>
