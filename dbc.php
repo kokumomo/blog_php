6d529a70b0ea6e28b408b46c612de5baa7ee0ec1
{km} <{k3042227625n@gmail.com}>
2023-10-09 07:42:54 +0900

    ブログ詳細画面表示

--- dbc.php
+++ dbc.php
@@ -74,6 +74,7 @@ function setCategoryName($category) {
             <td><?php echo $column['id'] ?></td>
             <td><?php echo $column['title'] ?></td>
             <td><?php echo setCategoryName($column['category']) ?></td>
+            <td><a href="/blog_php/detail.php?id=<?php echo $column['id'] ?>">詳細</a></td>
         </tr>
         <?php endforeach; ?>
     </table>
